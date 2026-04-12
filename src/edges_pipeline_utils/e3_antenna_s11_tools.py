455a50a46691a4aaa8fad6643fbdda33a9c7187b
akvydula <akshatha.vydula@gmail.com>
2026-04-11 19:39:35 -0700

    temperature log tools, AntS11 tools

---
+++ src/edges_pipeline_utils/e3_antenna_s11_tools.py
@@ -0,0 +1,115 @@
+'''
+Functions for testing Antenna S11 variabilities.
+'''
+from pathlib import Path
+import numpy as np
+from astropy import units as un
+import glob
+from edges.cal import ReflectionCoefficient, S11ModelParams
+from edges import modeling as mdl
+from edges.alanmode.alanmode import reads1p1
+from edges.frequencies import get_mask
+from edges_pipeline_utils import utils
+
+
+root_dir: Path = Path('/data5/edges/data/EDGES3_data/MRO') # this is where the raw files are
+alan_dir: Path = Path("/data4/vydula/edges/edges3_files/scripts/alan_300_310_tests/")
+datadir: Path = Path("/data4/vydula/edges/packages/edges3-data-analysis/data/")
+
+
+def calculate_rms(array, digits =3):
+    rms = np.sqrt(np.nanmean(array**2))
+    return round(rms, digits)
+
+
+def get_ant_s11(year, day, f_low=40, f_high=190, raw=False, n_terms =12, return_model = False):
+    try:
+        pattern_base = f"{root_dir}/{year}_{day:03d}_*"
+        Tfopen = glob.glob(f'{pattern_base}O.s1p')[0]
+        Tfshort = glob.glob(f'{pattern_base}S.s1p')[0]
+        Tfload = glob.glob(f'{pattern_base}L.s1p')[0]
+        Tfant = glob.glob(f'{pattern_base}ant.s1p')[0]
+    except IndexError:
+        # If any file is missing, return None
+        print(f"Missing one or more required files for year {year}, day {day}")
+        return None, None, None
+
+    try:
+        # reads1p1 returns a calibrated ReflectionCoefficient (not a (freq, s11) tuple).
+        gamma_ant = reads1p1(
+            res=49.930,
+            s11_file_open=Tfopen,
+            s11_file_short=Tfshort,
+            s11_file_load=Tfload,
+            s11_file_ant=Tfant,
+        )
+        raw_freq = gamma_ant.freqs
+        ea_ant_s11 = gamma_ant.reflection_coefficient
+
+    except ValueError:
+        # if the files are inconsistent
+        print("Cal files are inconsistent in frequencies")
+        return None, None, None
+
+    # get antenna temperature from temperature logger
+    file_name = Tfant.split('/')[-1]
+    print(file_name)
+    temperature = utils.extract_temperature(file_name)
+
+
+    mask = get_mask(raw_freq, f_low * un.MHz, f_high * un.MHz)
+
+    # use 53-105 MHz --> Alan sets wfstart/stop to 54-104 MHz but also hardcodes /pm 1 to it
+    ea_freq = raw_freq[mask]
+
+    ants11_raw = ReflectionCoefficient(
+        reflection_coefficient=ea_ant_s11[mask],
+        freqs=ea_freq,
+    )
+
+    if raw:
+        mod_freq = ea_freq
+    else:
+        mod_freq = np.arange(f_low, f_high, 0.390) * un.MHz  # B18 resolution is 390 kHz
+
+    ants11 = ants11_raw.smoothed(
+        params=S11ModelParams(
+            model=mdl.Polynomial(
+                n_terms=n_terms,
+                transform=mdl.Log10Transform(scale=(f_low + f_high) / 2),
+            ),
+            complex_model_type=mdl.ComplexRealImagModel,
+            set_transform_range=True,
+            fit_method="alan-qrd",
+            find_model_delay=True,
+        ),
+        freqs=mod_freq,
+    )
+
+    if return_model:
+        return ants11
+    else:
+        return temperature.value, mod_freq, ants11.reflection_coefficient
+
+
+
+def get_cut_off_freq_range(cut_off=-10, f_low=50, f_high=120, n_terms=16):
+    _, ref_freq, ref_ants11 = get_ant_s11(
+        year=2023, day=154, f_low=f_low, f_high=f_high, n_terms=n_terms
+    )
+    '''
+    Returns the frequency range based on S11 cut-off
+    '''
+
+    s11_dB = 20 * np.log10(np.abs(ref_ants11))
+
+    # this makes all the values close to zero, closest to zero will be the crossing point of the cut-off value
+    crossings = np.where(np.diff(np.sign(s11_dB - cut_off)))[0]
+
+    # # Find indices of the closest values
+    if len(crossings) >= 2:
+        first_index = crossings[0]
+        last_index = crossings[-1]
+
+    return ref_freq[first_index], ref_freq[last_index]
+
--- src/edges_pipeline_utils/utils.py
+++ src/edges_pipeline_utils/utils.py
@@ -7,8 +7,16 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta, time
 from pygsdata import GSData
+import astropy.units as un
 
+
+
+root_dir: Path = Path('/data5/edges/data/EDGES3_data/MRO/') # this is where the raw files are
+alan_dir: Path = Path("/data4/vydula/edges/edges3_files/scripts/alan_300_310_tests/")
+datadir: Path = Path("/data4/vydula/edges/packages/edges3-data-analysis/data/")
 
 def yday_to_alanday(year: int, day: int) -> int:
     """Convert (year, day-of-year) to Alan's continuous day index."""
@@ -130,3 +138,458 @@ def find_closest_calkit_stem(data_dir: str, year: int, day: int) -> str | None:
 
     closest_stem = min(stem_to_yd, key=lambda s: abs(day_offset(s)))
     return closest_stem
+
+
+
+def extract_temperature(
+        file_name,
+        load="box",
+        extract_log=False,
+        temperature_file=datadir / "temperature_data.csv",
+):
+    """
+    Take start and end time from the ancillary data and return the average temperature in that time range
+    --  For hot load, temperature from hot load temperature sensor is used (register 102) that
+    sits directly on the hot load at the end of 8 position switch
+    --  For amb, long cable open and short, the register 101 is used which is the temperature of the ambient load
+    """
+
+    # get the datetime object using file name
+
+    year = int(file_name[0:4])
+    doy = int(file_name[5:8])
+    hour = int(file_name[9:11])
+
+    start_time = datetime(year, 1, 1) + timedelta(days=doy - 1, hours=hour)
+
+    if extract_log == True:
+        """
+        extract the data from log only if required.
+        Default is False, meaning the pre-extracted file will be used
+        This is don to avoid repeted file parsing
+
+        """
+        temperature_file = extract_temp_values_from_logger(
+            "/data5/edges/data/EDGES3_data/MRO/temperature_logger/temperature.log"
+        )
+
+        # default file is temperature_data.csv
+
+    df = pd.read_csv(temperature_file)
+
+    df["Time"] = pd.to_datetime(df["Time"])
+
+    closest_row = df.loc[(df["Time"] - start_time).abs().idxmin()]
+
+    front_end_temp = closest_row["Front End temperature"]
+    amb_load_temp = closest_row["Amb load temperature"]
+    hot_load_temp = closest_row["Hot load temperature"]
+    inner_box_temp = closest_row["Inner box temperature"]
+    therm_control = closest_row["Thermal Control"]
+    battery_voltage = closest_row["Battery Voltage"]
+    battery_current = closest_row["PR59 Current"]
+
+    if load == "hot":
+        temperature = hot_load_temp
+        # print('hot', temperature)
+
+    elif load == "open" or "short" or "box" or "amb":
+        temperature = amb_load_temp  # this is default if no load is specified
+
+    if load == "amb":
+        temperature = amb_load_temp
+        # print('amb', temperature)
+
+    elif load == "hot":
+        temperature = hot_load_temp
+        # print('hot', temperature)
+
+    elif load == "open" or "short" or "box":
+        temperature = front_end_temp  # this is default if no load is specified
+        # print('default', temperature)
+
+    temperature_deg_C = temperature * un.deg_C
+    temperature_K = temperature_deg_C.to(un.K, equivalencies=un.temperature())
+
+    return temperature_K.mean()
+
+
+def filter_nighttime(datetimes, start_date, end_date, start_night=19, end_night=7):
+    """
+    Filters a list of datetime objects to include only those corresponding to night time observations
+
+    Args:
+        datetimes (list): List of datetime objects.
+        start_night (float): start of the night time, default is 19 hr
+        end_night (float): start of the night time, default is 7 hr
+
+    Returns:
+        list: List of datetime objects during nighttime.
+    """
+    nighttime = []
+    for dt in datetimes:
+
+        if dt >= start_date and dt <= end_date:
+            if dt.time() >= time(start_night, 0) or dt.time() < time(end_night, 0):
+                nighttime.append(dt)
+    return nighttime
+
+
+def extract_dates(anc_obj):
+    """
+    Take ancilliary data from acq file and return the start and end time as datetime objects.
+
+    """
+
+    start_time = anc_obj.data["times"][0][0].decode("utf-8")  # first instance of time
+    end_time = anc_obj.data["times"][-1][0].decode("utf-8")  # second instance of time
+
+    date_format = "%Y:%j:%H:%M:%S"
+
+    # Parse the string into a datetime object
+    start_time = datetime.strptime(start_time, date_format)
+
+    end_time = datetime.strptime(end_time, date_format)
+
+    return (start_time, end_time)
+
+
+def extract_temp_values_from_logger(
+        temperature_file="/data5/edges/data/EDGES3_data/MRO/temperature_logger/temperature.log",
+):
+    """
+    Easiest way (i think) is to read one line at a time, check conditions in each line,
+    extract a datetime object and temperature readouts.
+    I am throwing away the readouts that don't have complete information.
+    If such read out is encountered, count is 'reset' meaning that readout won't be appended to the dataframe
+
+    """
+
+    # lets first create a panda dataframe
+
+    df = pd.DataFrame(
+        {
+            "Time": [],
+            "Front End temperature": [],
+            "Amb load temperature": [],
+            "Hot load temperature": [],
+            "Inner box temperature": [],
+            "Thermal Control": [],
+            "Battery Voltage": [],
+            "PR59 Current": [],
+        }
+    )
+
+    with open(temperature_file) as file:
+        for line in file:
+            this_line = line
+            # print(this_line)
+
+            if "_" in this_line:
+                # print(this_line)
+
+                values = this_line.split(
+                    "_"
+                )  # this is the line that looks like 2022_318_03
+                if len(values) == 3:
+                    count = 0  # reset everytime the first line is encountered
+
+                    try:
+                        year = int(values[0])
+                        doy = int(values[1])
+                        count += 1
+                    except:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if "UTC" in this_line:
+                values = this_line.split(
+                    " "
+                )  # this_line is alredy split with single space
+
+                # this is the line that looks like "Mon Nov 14 03:20:26 UTC 2022"
+                # we only want time from this
+                if len(values) == 6:
+                    time = values[3]
+                    values = time.split(
+                        ":"
+                    )  # we take the time that looks like "03:20:26"
+
+                    try:
+                        hh = int(values[0])
+                        mm = int(values[1])
+                        ss = int(values[2])
+
+                        # create datetime object using year, day of the year, hh,mm,ss information.
+
+                        date_object = datetime(year, 1, 1, hh, mm, ss) + timedelta(
+                            days=doy - 1
+                        )
+                        count += 1
+
+                    except:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if (
+                    "0 " in this_line
+            ):  # this is the line that looks like "0 +3.000000e+01" --> we don't need this now
+                count += 1  # do nothing
+
+            if "100 " in this_line:
+                values = this_line.split(" ")  # this line is for sensor 100
+                if len(values) == 2:
+                    # check if there are two values in that line -- one for the sensor and one for the value
+
+                    try:
+                        front_end_box_temp = float(values[1].strip())
+                        count += 1
+                    except:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if "101 " in this_line:
+                values = this_line.split(" ")  # this line is for sensor 101
+                if (
+                        len(values) == 2
+                ):  # check if there are two values in that line -- one for the sensor and one for the value
+                    try:
+                        amb_load_temp = float(values[1].strip())
+                        count += 1
+
+                    except ValueError:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if "102 " in this_line:
+                values = this_line.split(" ")  # this line is for sensor 102
+                if len(values) == 2:
+                    try:
+                        hot_load_temp = float(values[1].strip())
+                        count += 1
+
+                    except ValueError:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if "103 " in this_line:
+                values = this_line.split(" ")  # this line is for sensor 103
+                if len(values) == 2:
+                    try:
+                        innerbox_temp = float(values[1].strip())
+                        count += 1
+
+                    except ValueError:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if "106 " in this_line:
+                values = this_line.split(" ")  # this line is for sensor 106
+                if len(values) == 2:
+                    try:
+                        therm_control = float(values[1].strip())
+                        count += 1
+
+                    except ValueError:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if "150 " in this_line:
+                values = this_line.split(" ")  # this is battery voltage 150
+                if len(values) == 2:
+                    try:
+                        battery_voltage = float(values[1].strip())
+                        count += 1
+
+                    except ValueError:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if "152 " in this_line:
+                values = this_line.split(" ")  # this is PR59 current 152
+                if len(values) == 2:
+                    try:
+                        pr59_current = float(values[1].strip())
+                        count += 1
+
+                    except ValueError:
+                        # skip this data record
+                        count = count  # do nothing
+
+            if count == 10:
+                temp_df = pd.DataFrame(
+                    {
+                        "Time": [date_object],
+                        "Front End temperature": [front_end_box_temp],
+                        "Amb load temperature": [amb_load_temp],
+                        "Hot load temperature": [hot_load_temp],
+                        "Inner box temperature": [innerbox_temp],
+                        "Thermal Control": [therm_control],
+                        "Battery Voltage": [battery_voltage],
+                        "PR59 Current": [pr59_current],
+                    }
+                )
+
+                # Concatenate the this DataFrame with the original DataFrame
+                df = pd.concat([df, temp_df], ignore_index=True)
+                count = 0  # reset
+
+    # for now I am saving it where the function is called, but we could find a more organized way of saving this file
+
+    df.to_csv(f"{datadir}/temperature_data.csv")
+
+    return f"{datadir}/temperature_data.csv"
+
+
+def extract_temperature(
+        file_name,
+        load="box",
+        extract_log=False,
+        temperature_file=datadir / "temperature_data.csv",
+):
+    """
+    Take start and end time from the ancillary data and return the average temperature in that time range
+    --  For hot load, temperature from hot load temperature sensor is used (register 102) that
+    sits directly on the hot load at the end of 8 position switch
+    --  For amb, long cable open and short, the register 101 is used which is the temperature of the ambient load
+    """
+
+    # get the datetime object using file name
+
+    year = int(file_name[0:4])
+    doy = int(file_name[5:8])
+    hour = int(file_name[9:11])
+
+    start_time = datetime(year, 1, 1) + timedelta(days=doy - 1, hours=hour)
+
+    if extract_log == True:
+        """
+        extract the data from log only if required.
+        Default is False, meaning the pre-extracted file will be used
+        This is don to avoid repeted file parsing
+
+        """
+        temperature_file = extract_temp_values_from_logger(
+            "/data5/edges/data/EDGES3_data/MRO/temperature_logger/temperature.log"
+        )
+
+        # default file is temperature_data.csv
+
+    df = pd.read_csv(temperature_file)
+
+    df["Time"] = pd.to_datetime(df["Time"])
+
+    closest_row = df.loc[(df["Time"] - start_time).abs().idxmin()]
+
+    front_end_temp = closest_row["Front End temperature"]
+    amb_load_temp = closest_row["Amb load temperature"]
+    hot_load_temp = closest_row["Hot load temperature"]
+    inner_box_temp = closest_row["Inner box temperature"]
+    therm_control = closest_row["Thermal Control"]
+    battery_voltage = closest_row["Battery Voltage"]
+    battery_current = closest_row["PR59 Current"]
+
+    if load == "hot":
+        temperature = hot_load_temp
+        # print('hot', temperature)
+
+    elif load == "open" or "short" or "box" or "amb":
+        temperature = amb_load_temp  # this is default if no load is specified
+
+    if load == "amb":
+        temperature = amb_load_temp
+        # print('amb', temperature)
+
+    elif load == "hot":
+        temperature = hot_load_temp
+        # print('hot', temperature)
+
+    elif load == "open" or "short" or "box":
+        temperature = front_end_temp  # this is default if no load is specified
+        # print('default', temperature)
+
+    temperature_deg_C = temperature * un.deg_C
+    temperature_K = temperature_deg_C.to(un.K, equivalencies=un.temperature())
+
+    return temperature_K.mean()
+
+
+# weather log is a big file so lets try to downselect to the days that we need
+
+
+def downselect_weatherlog(start_date, end_date, path='/data5/edges/data/2014_February_Boolardy/'):
+    """
+    Filters entries in the weather log text file by date range and saves them to a CSV file.
+
+    """
+
+    input_file_path = path + 'weather2.txt'
+
+    output_path = datadir / f"weather_log_{start_date}_to_{end_date}.txt"
+    start_datetime = datetime.strptime(start_date, "%Y_%j")
+    end_datetime = datetime.strptime(end_date, "%Y_%j")
+
+    with open(input_file_path, 'r') as txt_file, open(output_path, 'w') as out_file:
+
+        out_file.write("Datetime Rack_Temp(K) Ambient_Temp(K) Ambient_Hum(%) Frontend_Temp(K) RCV3_LNA_Temp(K)\n")
+
+        for line in txt_file:
+            parts = line.split()
+            try:
+                entry_datetime = datetime.strptime(parts[0], "%Y:%j:%H:%M:%S")
+            except ValueError:
+                continue
+
+            if start_datetime <= entry_datetime <= end_datetime:
+                out_file.write(
+                    f"{parts[0]} {float(parts[2])} {float(parts[5])} "
+                    f"{float(parts[8])} {float(parts[11])} {float(parts[14])}\n"
+                )
+
+    return output_path
+
+
+def get_weatherlog_closest_to_time(file_path, input_time):
+    """
+    Retrieve the log entry closest to the given input time.
+
+    Parameters:
+        file_path (str): Path to the weather log file.
+        input_time (str): Input time in the format "%Y:%j:%H:%M:%S".
+
+    Returns:
+        dict: A dictionary containing the closest weather log entry, or None if no entries exist.
+    """
+    from datetime import datetime
+
+    input_datetime = datetime.strptime(input_time, "%Y:%j:%H:%M:%S")
+    closest_entry = None
+    min_time_diff = float('inf')
+
+    with open(file_path, 'r') as file:
+        headers = file.readline().strip().split()  # Read headers
+
+        for line in file:
+            parts = line.strip().split()
+
+            if len(parts) != len(headers):
+                print(f"Skipping malformed line: {line.strip()}")
+                continue
+
+            entry_datetime = datetime.strptime(parts[0], "%Y:%j:%H:%M:%S")
+            time_diff = abs((entry_datetime - input_datetime).total_seconds())
+
+            if time_diff < min_time_diff:
+                min_time_diff = time_diff
+                closest_entry = parts
+
+    if closest_entry:
+        # print(f"Closest entry: {closest_entry}")
+        # Construct dictionary from the closest entry
+        return {
+            headers[i]: float(closest_entry[i]) if i > 1 else closest_entry[i]
+            for i in range(len(headers))
+        }
+    else:
+        return None
+
